ecc45a9b41e3984cb5d15e4e5fa7a37feb84de28
Kazuyuki Tanaka <52480444+kajiringo-blu@users.noreply.github.com>
2021-08-16 00:08:35 +0900

    Update header.php

--- ヘッダーフッター切り離し/header.php
+++ ヘッダーフッター切り離し/header.php
@@ -6,7 +6,7 @@
   <title>header_footer読み込み</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="ヘッダーとフッターを切り離して読み込ませる">
-  <link rel="stylesheet" href="header_footer.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>

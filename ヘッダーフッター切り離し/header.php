a6baa8852a19dd8a710250249a83cd26c12f448e
Kazuyuki Tanaka <52480444+kajiringo-blu@users.noreply.github.com>
2021-08-15 23:22:09 +0900

    Add files via upload

---
+++ ヘッダーフッター切り離し/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>header_footer読み込み</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="ヘッダーとフッターを切り離して読み込ませる">
+  <link rel="stylesheet" href="header_footer.css">
+</head>
+
+<body>
+  <header>
+    <h2>New Webpage ヘッダ</h2>
+  </header>
